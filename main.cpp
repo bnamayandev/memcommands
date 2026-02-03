f7a90e436a838ce37e7984cf6f54d32e20c2c8cf
bnamayandev <benjaminnamayandeh@gmail.com>
2026-02-02 20:43:15 -0500

    read from built-in unix history files

--- main.cpp
+++ main.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
 #include <string>
-
-// this is the node we used for the linked list
-struct CommandNode {
-   std::string command;
-   CommandNode* next;
-};
-
-void addCommand(CommandNode*& head, const std::string& cmd) {}
-
-void showHistory(CommandNode* head) {}
-
-void freeHistory(CommandNode*& head) {}
+#include <cstdlib>
+#include <fstream>
 
 int main() {
-   CommandNode* history = nullptr;
    std::string input;
+   std::string filePath;
 
-   std::cout << "Type 'history' or 'exit'\n";
+   //get the shell the user is using
+   std::string shellPath = std::getenv("SHELL");
+   std::string shellName = shellPath.substr(shellPath.find_last_of('/') + 1);
+   std::string home = std::getenv("HOME");
 
-   while (true) {
-      std::cout << "> ";
-      std::getline(std::cin, input);
-
-      if (input == "exit") {
-         break;
-      }
-
-      else if (input == "history") {
-         showHistory(history);
-         continue;
-      }
+   if (home.empty()) {
+      std::cerr << "HOME environment variable is not set";
+      return 1;
    }
 
-   freeHistory(history);
+   if (shellName == "bash") {
+       filePath = home + "/.bash_history";
+   } else if (shellName == "zsh") {
+       filePath = home + "/.zsh_history";
+   } else if (shellName == "fish") {
+       filePath = home + "/.local/share/fish/fish_history";
+   } else {
+       std::cerr << "Unsupported shell\n";
+       return 1;
+   } 
+   
+   // read this history file
+   std::ifstream file(filePath);
+
+   if (!file.is_open()) {
+      std::cerr << "Failed to open file: " << filePath << "\n";
+      return 1;
+   }
+
+   std::string line;
+   while (std::getline(file, line)) {
+      std::cout << line << '\n';
+   }
+   file.close();
+
    return 0;
 }
