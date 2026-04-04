612751ff3bbf72e8e76670f093615558d3bc2fe6
bnamayandev <benjaminnamayandeh@gmail.com>
2026-04-03 20:05:19 -0400

    Added basic main to demonstrate functionality

--- tui/main.go
+++ tui/main.go
@@ -2,7 +2,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"memcommands/core"
 )
 
@@ -12,11 +15,27 @@ type model struct {
 
 func main() {
 	lines, err := core.GetHistoryLines();
+	reader := bufio.NewReader(os.Stdin);
 	
 	if err != nil {
 		fmt.Println(err) 
 		return
 	}
 
-	fmt.Println(lines);
+	fmt.Println("Enter input: ");
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("There was an error reading the input: ")
+		return
+	}
+
+	input = strings.TrimSpace(input)
+	fmt.Printf("You entered: %s\n", input)
+	
+	res := core.GetFuzzyScoreList(lines, input);
+
+	for _, val := range res {
+		fmt.Println(val)
+	}
 }
